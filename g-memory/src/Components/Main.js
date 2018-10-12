2cc2f6e303e82c297501ba5f3e4813b307e38bb9
Brandon Redshirt <brandon.redshirt@gmail.com>
2018-10-12 14:21:19 -0600

    styling Main

--- g-memory/src/Components/Main.js
+++ g-memory/src/Components/Main.js
@@ -5,7 +5,8 @@ class Main extends React.Component{
     
     render (){
         return(
-            <div className="body container">
+            <div className="body container-fluid">
+                <h1 className="gTitle">gMemory</h1>
                 <Card data={this.props.data}/>
 
             </div>
